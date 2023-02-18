5014540f21d4789eaffb516f4c078f89723ca502
mihayounas <76411576+mihayounas@users.noreply.github.com>
2023-02-18 00:53:19 +0000

    fix font awwsome icons

--- air-go/src/components/NavBar.js
+++ air-go/src/components/NavBar.js
@@ -3,32 +3,23 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+
+
 function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home"><i className="fa-plane-arrival"></i>AirGo</Navbar.Brand>
+        <Navbar.Brand href="#home"><i class="fa-sharp fa-solid fa-plane-departure"></i>AirGo</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#features">Flights</Nav.Link>
+            <Nav.Link href="#pricing">Car Hire</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
+
             <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+              Log In
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
