562c7477512c3b82835272dedd6e97d9f7001307
mihayounas <76411576+mihayounas@users.noreply.github.com>
2023-02-18 23:16:16 +0000

    put in place popular locations and research skyscanner api

--- air-go/src/App.js
+++ air-go/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import FlightSearchPage from "./pages/FlightSearchPage";
+import PopularLocations from "./components/PopularLocations"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <FlightSearchPage />
+      <PopularLocations />
     </div>
   );
 }
---
+++ air-go/src/components/PopularLocations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function PopularLocations() {
+  return (
+    <div className="container">
+      <h1 style={{color:"white"}}>Popular Locations Section</h1>
+    </div>
+  );
+}
+
+export default PopularLocations;
