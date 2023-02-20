063ee79105f16e2898942cca53911ac2ce25767c
mihayounas <76411576+mihayounas@users.noreply.github.com>
2023-02-20 21:43:48 +0000

    work on popular locations section

--- air-go/src/components/PopularLocations.js
+++ air-go/src/components/PopularLocations.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const PopularLocations = () => {
+  const [popularDestinations, setPopularDestinations] = useState([]);
 
-function PopularLocations() {
+  const SKYSCANNER_API_KEY = 'YOUR_API_KEY_HERE';
+
+  const getPopularDestinations = async () => {
+    const response = await axios.get(`https://partners.api.skyscanner.net/apiservices/reference/v1.0/countries/en-US?apiKey=${SKYSCANNER_API_KEY}`);
+    setPopularDestinations(response.data.Countries);
+  };
+
+  useEffect(() => {
+    getPopularDestinations();
+  }, []);
+
   return (
-    <div className="container">
-      <h1 style={{color:"white"}}>Popular Locations Section</h1>
+    <div>
+      <h2>Popular Locations</h2>
+      <ul>
+        {popularDestinations.map((destination) => (
+          <li key={destination.Id}>
+            <img src={`https://content.r9cdn.net/rimg/dimg/${destination.ImageUrl}`} alt={destination.Name} />
+            {destination.Name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default PopularLocations;
